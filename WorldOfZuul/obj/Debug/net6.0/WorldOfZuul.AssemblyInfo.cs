e0b9fe347968ac6d1b217ed47a0a0971c86c092f
RoustyTousty <emils.misisons@gmail.com>
2025-10-10 11:57:33 +0200

    Updated init

--- WorldOfZuul/obj/Debug/net6.0/WorldOfZuul.AssemblyInfo.cs
+++ WorldOfZuul/obj/Debug/net6.0/WorldOfZuul.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bd560b6a30e8e0c10b1a44bb84348213ffc818a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f856943fcaf2955b03cd810fe1da8242d41c3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
