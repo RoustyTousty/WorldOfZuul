aaf47066658e9a2006416a8b2344f2f306ffa202
MartinSmolek <msmolek.dk@gmail.com>
2025-10-10 15:58:42 +0200

    Test hi
    Hello!

--- WorldOfZuul/commands/Command.cs
+++ WorldOfZuul/commands/Command.cs
@@ -14,7 +14,7 @@ namespace WorldOfZuul
         public Command(string name, string? secondWord = null)
         {
             Name = name;
-            SecondWord = secondWord;
+            SecondWord = secondWord;   //change test comment hi :333 
         }
     }
 }
