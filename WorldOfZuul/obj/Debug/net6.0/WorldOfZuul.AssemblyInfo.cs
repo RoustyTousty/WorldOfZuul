893bd0c642f28b783268d91efa828e62be71b320
RoustyTousty <emils.misisons@gmail.com>
2025-10-10 15:37:27 +0200

    Updated gitignore

--- WorldOfZuul/obj/Debug/net6.0/WorldOfZuul.AssemblyInfo.cs
+++ WorldOfZuul/obj/Debug/net6.0/WorldOfZuul.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67175771232c6fabeedb213838e4c491560afad8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f856943fcaf2955b03cd810fe1da8242d41c3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
