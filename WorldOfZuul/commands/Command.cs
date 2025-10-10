5432c50d9b8c4a829f27c8f334b05c6f755dd358
Chiaracianca <chcia25@student.sdu.dk>
2025-10-10 16:08:02 +0200

    Update Command.cs

--- WorldOfZuul/commands/Command.cs
+++ WorldOfZuul/commands/Command.cs
@@ -14,11 +14,13 @@ namespace WorldOfZuul
         public Command(string name, string? secondWord = null)
         {
             Name = name;
+<<<<<<< HEAD
 <<<<<<< Updated upstream:WorldOfZuul/commands/Command.cs
             SecondWord = secondWord;
 =======
             SecondWord = secondWord;//daje roma
 >>>>>>> Stashed changes:WorldOfZuul/Command.cs
+=======
             SecondWord = secondWord;   //change test comment hi :333 
 >>>>>>> aaf47066658e9a2006416a8b2344f2f306ffa202
         }
